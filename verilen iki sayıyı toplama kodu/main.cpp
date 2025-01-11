70205bff8027e1dc24ac3e8dd04b6f2df7ce44d8
sami celebi <sam74.celebi@gmail.com>
2025-01-11 17:23:58 +0300

    toplama

---
+++ verilen iki sayıyı toplama kodu/main.cpp
@@ -0,0 +1,15 @@
+ #include <iostream>
+  using namespace std;
+
+  int main() {
+int sayi1;
+int sayi2;
+int toplam;
+cout<<"Ilk sayiyi giriniz:\n";
+cin>>sayi1;
+cout<<"Ikinci sayiyi giriniz:\n";
+cin>>sayi2;
+toplam = sayi1 + sayi2;
+cout<<"Girilen iki sayinin toplami: "<<toplam<<endl;
+return 0; }
+  
